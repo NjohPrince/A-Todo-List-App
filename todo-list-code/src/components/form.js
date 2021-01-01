e7421d62af8949b6038da47d0ec49586987af077
NjohPrince <jufredprince@gmail.com>
2021-01-01 08:47:25 +0100

    created a form to be filled to add todo list elements

---
+++ todo-list-code/src/components/form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+      <form>
+        <input type="text" className="todo-input" />
+        <button className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    );
+};
+
+export default Form;
