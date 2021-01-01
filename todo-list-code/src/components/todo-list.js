42e1c3104c9bcc5ea1cd183aa576a0e5f16f7306
NjohPrince <jufredprince@gmail.com>
2021-01-01 10:47:02 +0100

    styled the todo items

--- todo-list-code/src/components/todo-list.js
+++ todo-list-code/src/components/todo-list.js
@@ -5,7 +5,8 @@ const TodoList = () => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-          <Todo />
+        <Todo />
+        <Todo />
       </ul>
     </div>
   );
