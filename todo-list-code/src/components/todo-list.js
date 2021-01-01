3f4c701a227b72de5620248a72b73d086970d058
NjohPrince <jufredprince@gmail.com>
2021-01-01 10:20:09 +0100

    created a parent component for the todo list items

---
+++ todo-list-code/src/components/todo-list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from 'Todo';
+
+const TodoList = () => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+          <Todo />
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
