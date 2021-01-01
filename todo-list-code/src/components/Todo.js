327c9ed5dadf5eea39d4d83fea774d1239922766
NjohPrince <jufredprince@gmail.com>
2021-01-01 11:05:41 +0100

    added the input text to the todo list

--- todo-list-code/src/components/Todo.js
+++ todo-list-code/src/components/Todo.js
@@ -1,11 +1,11 @@
 import React from 'react';
  
-const Todo = () => {
+const Todo = ({text}) => {
   return (
     <div className="todo">
-        <li className="todo-item">Heyyy</li>
-        <button className="complete-btn">Done</button>
-        <button className="delete-btn">Delete</button>
+      <li className="todo-item">{text}</li>
+      <button className="complete-btn">Done</button>
+      <button className="delete-btn">Delete</button>
     </div>
   );
 };
--- todo-list-code/src/components/todo-list.js
+++ todo-list-code/src/components/todo-list.js
@@ -4,9 +4,10 @@ import Todo from './Todo';
 const TodoList = ({todos}) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list">{todos.map(todo => (
-        <Todo />
-      ))}
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo text={todo.text}/>
+        ))}
       </ul>
     </div>
   );
