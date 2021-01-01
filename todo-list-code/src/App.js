a7968d3160f8e424564951b004e8bc892b48fe42
NjohPrince <jufredprince@gmail.com>
2021-01-01 10:33:19 +0100

    styling the list of items and rendered it on the screen via App.js

--- todo-list-code/src/App.js
+++ todo-list-code/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './components/form';
+import TodoList from './components/todo-list';
 
 function App() {
   const [inputText, setInputText] = useState('');
@@ -11,6 +12,7 @@ function App() {
         <h1>Strawby's Todo List</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
---
+++ todo-list-code/src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const Todo = () => {
+  return (
+    <div className="todo">
+        <li className="todo-item">Heyyy</li>
+        <button className="complete-btn">Done</button>
+        <button className="delete-btn">Delete</button>
+    </div>
+  );
+};
+
+export default Todo;
