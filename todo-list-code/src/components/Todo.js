85b744a68fa0ebd297c17f593a503cc1f5e7dde4
NjohPrince <jufredprince@gmail.com>
2021-01-01 13:40:29 +0100

    working on the completed button

--- todo-list-code/src/components/Todo.js
+++ todo-list-code/src/components/Todo.js
@@ -4,10 +4,15 @@ const Todo = ({text, todo, todos, setTodos}) => {
   const deleteHandler = () => {
      setTodos(todos.filter(el => el.id !== todo.id))
   }
+  const completeHandler = () => {
+    setTodos(todos.map((item) => {
+      
+    }));
+  };
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
-      <button className="complete-btn">Done</button>
+      <button onClick={completeHandler} className="complete-btn">Done</button>
       <button onClick={deleteHandler} className="delete-btn">Delete</button>
     </div>
   );
