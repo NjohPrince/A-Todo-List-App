ca7f234a9ec5663857be01710c85f5339e4b66f1
NjohPrince <jufredprince@gmail.com>
2021-01-04 23:20:27 +0100

    passed the setStatus down to the form

--- todo-list-code/src/App.js
+++ todo-list-code/src/App.js
@@ -12,7 +12,13 @@ function App() {
       <header>
         <h1>Strawby's Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        setStatus={setStatus}
+      />
       <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
